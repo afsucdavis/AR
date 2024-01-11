3323533a9185f7d0eedcda03ca601f87c1ebfe92
AFSUCDAVIS <44675955+afsucdavis@users.noreply.github.com>
2024-01-11 10:42:40 -0800

    new files
    new files

---
+++ story_content/5dMTUdZ4stT_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.65.28121.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.180%0D%0APlease%20click%20the%20links%20available%20on%20this%20menu%20to%20become%20familiar%20with%20these%20search%20pages.%0D%0A%0D%0A"}')
